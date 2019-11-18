5ac1a60631abd7e89e4f337df267cf3909889c43
Kenny Callaghan <kcallaghan@ithaca.edu>
2019-11-18 08:27:00 -0500

    Wrote ADT for Queue

---
+++ Queue.h
@@ -0,0 +1,30 @@
+//
+// Created by Kenny on 11/15/2019.
+//
+
+#ifndef TERMPROJECT_QUEUE_H
+#define TERMPROJECT_QUEUE_H
+
+#include "Song.h"
+
+class Queue{
+private:
+    // Abstract, should not be directly implemented.
+    Queue(const Queue& queueToCopy);
+    Queue& operator=(const Queue& queueToCopy);
+
+public:
+    // constructor
+    Queue() {}
+
+    // destructor
+    virtual ~Queue() {}
+
+    // adds new Song to a queue, appending to the end of the queue.
+    virtual void enqueue(Song& songToAdd)=0;
+
+    // removes a song from the front of the queue, returns the song.
+    virtual Song dequeue() = 0;
+};
+
+#endif //TERMPROJECT_QUEUE_H
